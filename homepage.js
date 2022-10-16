6e190133202f324801be4675942115f15cb061c2
Vasudha Gabhane <98514415+VasudhaMGabhane@users.noreply.github.com>
2022-10-16 10:12:46 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,18 +1,48 @@
+// hamburger icon
 function myFunction() {
-  var x = document.getElementById("myLinks");
-  var y = document.getElementById("icon");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "none") {
     x.style.display = "block";
-  
+    return false;
+  } else {
+    x.style.display = "none";
+    
   }
+  
 }
 
 // coupon
 window.onload = function(){
   document.getElementById('close').onclick = function(){
-   this.parentNode.parentNode.remove();
+   this.parentNode.parentNode.style.display = "none";
    return false;
   }
+
+  document.getElementById('couponopen').onclick = function(){
+    this.parentNode.parentNode.style.display = "block";
+   
+  }
+}
+
+// dark mode
+const colorSwitch = document.getElementById('input-color-switch');
+
+colorSwitch.addEventListener('click',checkMode);
+
+function checkMode(){
+  console.log('checking');
+  if(colorSwitch.checked){
+    console.log('dark on');
+    darkModeOn();
+  }
+   else{
+    console.log('dark off');
+    darkModeOff();
+   }
+}
+function darkModeOn(){
+  document.body.classList.add("dark-mode");
+}
+function darkModeOff(){
+  document.body.classList.remove("dark-mode");
 }
